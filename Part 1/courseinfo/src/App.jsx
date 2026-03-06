b9e91d2fadf61ebbeeb36111d8a40fcfb1bc2164
Niki Najeroo <njrclass@gmail.com>
2026-03-06 23:53:25 +0700

    Exercise 1.1 & 1.2 Completion

---
+++ Part 1/courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+  const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    return (
+      <div>
+        <Header courseName={course}/>
+        <Content 
+          part1={part1} exercises1={exercises1}
+          part2={part2} exercises2={exercises2}
+          part3={part3} exercises3={exercises3} 
+        />
+        <Total a={exercises1} b={exercises2} c={exercises3} />
+      </div>
+    )
+  }
+
+  const Header = (props) => {
+    console.log(props)
+    return (
+      <div>
+        <h1>{props.courseName}</h1>
+      </div>
+    )
+  }
+
+  const Content = (props) => {
+    console.log(props)
+    return (
+      <div>
+        <Part name={props.part1} value = {props.exercises1}/>
+        <Part name={props.part2} value = {props.exercises2}/>
+        <Part name={props.part3} value = {props.exercises3}/>
+      </div>
+    )
+  }
+
+  const Part = (props) => {
+    console.log(props)
+    return (
+      <div>
+        <p>{props.name}: {props.value}</p>
+      </div>
+    )
+  }
+  const Total = (props) => {
+    console.log(props)
+    return (
+      <div>
+        <p>Total Courses: {props.a + props.b + props.c}</p>
+      </div>
+    )
+  }
+
+
+
+  export default App
